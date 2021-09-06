e7bf57cc4fbd5aa0614dd9fbe16aedde110dff9c
Tomás <tomas.seguram@gmail.com>
2021-09-05 22:27:26 -0600

    cambios

---
+++ Tarea01/test_funtras.cpp
@@ -0,0 +1,10 @@
+#include "funtras.h"
+
+using namespace std;
+
+int main(){
+    double base = ( sin_t(3 * divt(7)) + ln_t(2));
+    double result =  (divt(cos_t(sqrt_t(2))) * (root_t(base, 3))) + log_t(pi(), exp_t(-1));
+    cout<< "result: " << result << endl;
+    return result;
+}
